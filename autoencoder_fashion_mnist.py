22a5cd3300215f3a4b5de5bbe394cde84a7ee35d
Po-wei Huang <poweihuang17@gmail>
2017-11-28 12:48:28 +0800

    Start to load data.

--- autoencoder_fashion_mnist.py
+++ autoencoder_fashion_mnist.py
@@ -3,3 +3,9 @@ from __future__ import division, print_function, absolute_import
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
+
+import scipy.io
+f_mnist = scipy.io.loadmat(‘fashion_mnist_dataset.mat’)
+
+#Get the data and labels using
+f_mnist[‘X_train’], f_mnist[‘y_train’], f_mnist[‘X_test’], f_mnist[‘y_test’]
